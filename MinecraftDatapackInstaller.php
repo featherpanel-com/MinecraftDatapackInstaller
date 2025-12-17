3292442944a1b29b2d79f27740498d5bfbff00ac
NaysKutzu <ghermancassian2008@gmail.com>
2025-12-17 16:25:15 +0100

    Sync

--- MinecraftDatapackInstaller.php
+++ MinecraftDatapackInstaller.php
@@ -1,32 +1,51 @@
 <?php
 
+/*
+ * This file is part of FeatherPanel.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 MythicalSystems
+ * Copyright (c) 2025 Cassian Gherman (NaysKutzu)
+ * Copyright (c) 2018 - 2021 Dane Everitt <dane@daneeveritt.com> and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace App\Addons\minecraftdatapackinstaller;
 
 use App\Plugins\AppPlugin;
 
 class MinecraftDatapackInstaller implements AppPlugin
 {
-    /**
-     * @inheritDoc
-     */
     public static function processEvents(\App\Plugins\PluginEvents $event): void
     {
         // Process plugin events here
         // Example: $event->on('app.boot', function() { ... });
     }
 
-    /**
-     * @inheritDoc
-     */
     public static function pluginInstall(): void
     {
         // Plugin installation logic
         // Create tables, directories, etc.
     }
 
-    /**
-     * @inheritDoc
-     */
     public static function pluginUpdate(?string $oldVersion, ?string $newVersion): void
     {
         // Plugin update logic
@@ -35,9 +54,6 @@ class MinecraftDatapackInstaller implements AppPlugin
         // $newVersion contains the new version being installed (e.g., '1.0.1')
     }
 
-    /**
-     * @inheritDoc
-     */
     public static function pluginUninstall(): void
     {
         // Plugin uninstallation logic
